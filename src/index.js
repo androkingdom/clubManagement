1b49e4fd11541a6f74e9765aaff644d6fd91ccf3
androkingdom <androkingdom1@gmail.com>
2025-03-07 21:46:12 +0530

    change port

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const Todos = ["Extrace Use full function in admin controlle to Utils", ""];
 
 connectDB()
   .then(() => {
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
       console.log(`⚙️ Server is running on http://localhost:${port}`);
       console.log(Todos);
     });
