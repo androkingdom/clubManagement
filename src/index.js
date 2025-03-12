74ed17d24399a77a1ac85cd0455e27493061cc77
androkingdom <androkingdom1@gmail.com>
2025-03-12 20:44:34 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import connectDB from "./db/index.db.js";
 
 dotenv.config({ path: "./.env" });
 
-const port = process.env.PORT ?? 8000;
+const port = process.env.PORT || 8000;
 
 const Todos = ["Extrace Use full function in admin controlle to Utils", ""];
 
 connectDB()
   .then(() => {
-    app.listen(port, "0.0.0.0", () => {
+    app.listen(port, process.env.PORT_ORIGIN, () => {
       console.log(`⚙️ Server now running on http://localhost:${port}`);
       console.log(Todos);
     });
